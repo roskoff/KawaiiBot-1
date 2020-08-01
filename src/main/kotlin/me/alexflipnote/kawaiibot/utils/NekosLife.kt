100c4d24418b51ec91d0e1f4b8eee58ffbfb659b
hitomi <43500169+nanami-hitomi@users.noreply.github.com>
2020-08-01 10:11:42 -0400

    or not

--- src/main/kotlin/me/alexflipnote/kawaiibot/utils/NekosLife.kt
+++ src/main/kotlin/me/alexflipnote/kawaiibot/utils/NekosLife.kt
@@ -9,9 +9,9 @@ object NekosLife{
 
     private val END_URLS = ResourceUtil.readJson<HashMap<String?,String?>>("Resources/nekosLifeEndpoints.json")
 
-    inline fun findImage(type: String) : String{
+    fun findImage(type: String) : String{
         val end=END_URLS.get(type)
-        val res
+        val res={}
         RequestUtil.get("https://api.nekos.dev/api/v3/images/sfw/$end").thenAccept {
             res = it.json()?.getString("url")
 
